eeac5b45638c1889443894585f41ec8c3774b62c
jose <josealvarres28@gmail.com>
2023-08-17 09:57:46 -0600

    Se agrega ventanas

--- index.js
+++ index.js
@@ -12,19 +12,58 @@ class UI {
     contenedor.classList.add ("bgcolore-success");
 
     contenedor.innerHTML = `
-        <div class="alert alert-primary d-flex mensaje ct" role="alert" >
-            <p>${producto.nombre}</p>
-            <p>${producto.cantidad}</p>
-            <p>${producto.año}</p>
+
+<div class="alert alert-primary d-flex flex-column mensaje ct" role="alert">
+    <div class="d-flex align-items-start">
+       <div class="d-flex">
+        <p class="p-2 ">${producto.nombre}</p>
+        <p class="p-2 ">${producto.cantidad}</p>
+        <p class="p-2 ">${producto.año}</p>
         </div>
+        <div class=" ms-auto ">
+        <a class="btn btn-danger" id="eliminar">Eliminar</a>
+        </div>
+    </div>
+</div>
     `;
     const ventana = document.getElementById("ventanaemergente");
     ventana.prepend(contenedor);
+
+
+    this.showmessage ();
   }
 
+  //Permite resetear los datos del formulario
   resetear (){
     document.getElementById("formulario").reset();
   }
+
+  delete () {
+    const eliminar = document.getElementById("eliminar");
+    eliminar.addEventListener("click", ()=>{
+        eliminar.parentNode.parentNode.parentNode.remove ();
+    });
+  }
+showmessage () {
+    const principal = document.getElementById("principal");
+    const mensaje = document.createElement ("div");
+    mensaje.innerHTML = `
+    <div class="alert alert-success" role="alert">
+    Se agrego el producto de forma correcta
+  </div>
+    ` 
+   
+    principal.prepend(mensaje);
+
+    setTimeout (()=>{
+        mensaje.remove ();
+
+    },1000);
+
+
+}
+
+
 }
 
 // DOM
@@ -40,6 +79,7 @@ document
     const ui = new UI();
 
     ui.addProduct(producto);
+    ui.delete ();
 
     event.preventDefault();
 
